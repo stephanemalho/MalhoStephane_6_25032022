3f9147c4176f5b7eff0e02aab5b24dda6e369adb
stephane.malho <stepahen.malho@gmail.com>
2022-03-30 20:53:31 +0200

    configure server.js  and delete other  files

--- server.js
+++ server.js
@@ -13,29 +13,14 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Response OK." });
-});
-require("./app/routes/tutorial.routes")(app);
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
 
 
 
