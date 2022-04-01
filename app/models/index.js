88f2f7b56292cde9b644774fe9b3a736a22114f4
stephane.malho <stepahen.malho@gmail.com>
2022-04-01 11:46:35 +0200

    add url in index.js

--- app/models/index.js
+++ app/models/index.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = 'mongodb+srv://P6openclassrooms:P6openclassrooms@cluster0.pfz4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 db.tutorials = require("./tutorial.model.js")(mongoose);
 module.exports = db;
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require("express"); // import express
 const bodyParser = require("body-parser");
 const cors = require("cors"); // import cors
 const app = express();
-const mongoose = require("mongoose");
-
+// const mongoose = require("mongoose");
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -16,8 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // set port, listen for requests
 const db = require("./app/models");
-
 // set port, listen for requests
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
@@ -27,10 +37,4 @@ app.get("/", (req, res) => {
   res.json({ message: "Response OK." });
 });
 
-//connect to databas mongoDB
-mongoose.connect('mongodb+srv://P6openclassrooms:P6openclassrooms@cluster0.pfz4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
