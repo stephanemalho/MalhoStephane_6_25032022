3ea646947a0aa300b10adcbca081cefec7c56ceb
stephane.malho <stepahen.malho@gmail.com>
2022-04-01 11:34:36 +0200

    connect to databas mongoDB atlas

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express"); // import express
 const bodyParser = require("body-parser");
 const cors = require("cors"); // import cors
 const app = express();
+const mongoose = require("mongoose");
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -13,6 +15,9 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 // set port, listen for requests
+const db = require("./app/models");
+
+// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
@@ -22,6 +27,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Response OK." });
 });
 
-
-
+//connect to databas mongoDB
+mongoose.connect('mongodb+srv://P6openclassrooms:P6openclassrooms@cluster0.pfz4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
