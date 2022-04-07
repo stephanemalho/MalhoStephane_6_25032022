5219d081f6ba181582a3fb91f4e7d6dad9202c51
stephane.malho <stepahen.malho@gmail.com>
2022-04-07 23:05:37 +0200

    create sauce model

--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: [true, "Champ requis"], match: [/\S+@\S+\.\S+/, 'Invalide'],lowercase: true, unique: true },
-  password: { type: String, required: true }
+  email: 
+    { 
+      type: String, 
+      required: [true, "Champ requis"], 
+      lowercase: true, 
+      unique: true,
+      trim: true,
+      maxlength: 64
+    },
+  password: 
+    { type: String, 
+      required: [true, "Mot de passe requis"],
+      trim: true,
+      minLength: [6, "Le mot de passe doit contenir au mois 6 caractères"],
+      minNumbers: 1,
+      minLowercase: 1,
+      minUppercase: 1,
+    }
 });
 
 UserSchema.plugin(uniqueValidator, {message: 'Éxiste déja.'});
